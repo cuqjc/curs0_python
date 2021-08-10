77538f43a72d585d4460337a6de7e195e7ca90ae
cuqjc <Cuquejo.juan97@gmail.com>
2021-08-10 19:38:45 -0300

    Cambion en archivo para practicar los dic comprehensions

--- reto_clase10.py
+++ reto_clase10.py
@@ -1,12 +1,11 @@
 def run():
     x = 0
-    for i in range(1,101):
-        number = {i: i ** 3}
-        print(number)
+#    for i in range(1,101):
+#        number = {i: i ** 3}
+#        print(number)
 
-    diccionario = {i for i in range(1, 101)}
-    print(diccionario)
-
+    my_dic={i: round(i**0.5, 2) for i in range(1,1001)}
+    print(my_dic)
 
 if __name__ == '__main__':
     run()
